9d5658599f8953f0b57bff4bc504383c5cb44338
liheying <85384255@qq.com>
2024-08-14 18:38:08 +0800

    add put & get

--- internal/tsh/tsh.go
+++ internal/tsh/tsh.go
@@ -6,9 +6,11 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	pel "tsh-go/internal/rsh"
 
+	"github.com/schollz/progressbar/v3"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -16,12 +18,14 @@ func Run() {
 	flagset := flag.NewFlagSet(filepath.Base(os.Args[0]), flag.ExitOnError)
 	flagset.Usage = func() {
 		fmt.Fprintf(flagset.Output(), "Usage: ./%s uuid\n", flagset.Name())
+		fmt.Fprintf(flagset.Output(), "        uuid get <source-file> <dest-dir>\n")
+		fmt.Fprintf(flagset.Output(), "        uuid put <source-file> <dest-dir>\n")
 		flagset.PrintDefaults()
 	}
 	flagset.Parse(os.Args[1:])
 
 	args := flagset.Args()
-	var uuid, command string
+	var uuid, command, srcfile, dstdir string
 	var mode uint8
 
 	if len(args) == 0 {
@@ -36,6 +40,14 @@ func Run() {
 	switch {
 	case len(args) == 0:
 		mode = pel.RunShell
+	case args[0] == "get" && len(args) == 3:
+		mode = pel.GetFile
+		srcfile = args[1]
+		dstdir = args[2]
+	case args[0] == "put" && len(args) == 3:
+		mode = pel.PutFile
+		srcfile = args[1]
+		dstdir = args[2]
 	default:
 		mode = pel.RunShell
 		command = args[0]
@@ -44,13 +56,78 @@ func Run() {
 
 	layer, err := pel.Dial(uuid, pel.PEL_SECRET, false)
 	if err != nil {
-		fmt.Printf("Authentication failed: %v\n", err)
+		fmt.Printf("边缘设备连接失败: %v\n", err)
 		os.Exit(0)
 	}
 	defer layer.Close()
 	layer.Write([]byte{mode})
 
-	handleRunShell(layer, command, use_ps1)
+	switch mode {
+	case pel.RunShell:
+		handleRunShell(layer, command, use_ps1)
+	case pel.GetFile:
+		handleGetFile(layer, srcfile, dstdir)
+	case pel.PutFile:
+		handlePutFile(layer, srcfile, dstdir)
+	}
+}
+
+func handleGetFile(layer *pel.PktEncLayer, srcfile, dstdir string) {
+	buffer := make([]byte, pel.Bufsize)
+
+	basename := strings.ReplaceAll(srcfile, "\\", "/")
+	basename = filepath.Base(filepath.FromSlash(basename))
+
+	f, err := os.OpenFile(filepath.Join(dstdir, basename), os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	_, err = layer.Write([]byte(srcfile))
+	if err != nil {
+		return
+	}
+	bar := progressbar.NewOptions(-1,
+		progressbar.OptionSetWidth(20),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("Downloading"),
+		progressbar.OptionSpinnerType(22),
+	)
+	io.CopyBuffer(io.MultiWriter(f, bar), layer, buffer)
+	fmt.Print("\nDone.\n")
+}
+
+func handlePutFile(layer *pel.PktEncLayer, srcfile, dstdir string) {
+	buffer := make([]byte, pel.Bufsize)
+	f, err := os.Open(srcfile)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return
+	}
+	fsize := fi.Size()
+
+	basename := filepath.Base(srcfile)
+	basename = strings.ReplaceAll(basename, "\\", "_")
+	_, err = layer.Write([]byte(dstdir + "/" + basename))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bar := progressbar.NewOptions(int(fsize),
+		progressbar.OptionSetWidth(20),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("Uploading"),
+	)
+	io.CopyBuffer(io.MultiWriter(layer, bar), f, buffer)
+	fmt.Print("\nDone.\n")
 }
 
 func handleRunShell(layer *pel.PktEncLayer, command string, use_ps1 bool) {
